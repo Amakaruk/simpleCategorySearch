a29a0d52cab405648b2065e80e20edc3b9e46208
Aaron <aaronmakaruk@proton.me>
2024-09-28 16:36:17 -0600

    service worker update

--- app.js
+++ app.js
@@ -63,8 +63,8 @@ if ('serviceWorker' in navigator) {
         .then(registration => {
             console.log('Service Worker registered with scope:', registration.scope);
         })
-        .catch(err => {
-            console.log('Service Worker registration failed:', err);
+        .catch(error => {
+            console.error('Service Worker registration failed:', error);
         });
     });
 }
